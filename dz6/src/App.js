b1e418324d982915324aae26a3e5609091254405
Aicholpon98 <aybekova18@gmail.com>
2025-03-27 18:47:09 +0600

    Add files via upload

---
+++ dz6/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Forms from "./components/Forms";
+
+function App() {
+    return (
+        <div>
+            <h1>Форма ввода ФИ </h1>
+            <Forms />
+        </div>
+    );
+}
+
+export default App;
+
+
